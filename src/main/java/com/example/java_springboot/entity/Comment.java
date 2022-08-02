a8315ad7c4e395ab38ad164fad5bfc85a8fee4d8
huytqth2008010 <70880121+huytqth2008010@users.noreply.github.com>
2022-08-02 18:12:47 +0700

    update order, order detail, bill, bill detail, slide

--- src/main/java/com/example/java_springboot/entity/Comment.java
+++ src/main/java/com/example/java_springboot/entity/Comment.java
@@ -1,9 +1,12 @@
 package com.example.java_springboot.entity;
 
 import com.example.java_springboot.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -15,4 +18,18 @@ import javax.persistence.Table;
 @Table(name = "comments")
 public class Comment extends BaseEntity {
     private String content;
+    private  long id_account;
+    private int id_product;
+
+    @ManyToOne
+    @JoinColumn(name = "id_account", insertable = false, updatable = false)
+    @JsonIgnore
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product", insertable = false, updatable = false)
+    @JsonIgnore
+    private Product product;
+
+
 }
--- src/main/java/com/example/java_springboot/entity/Reply.java
+++ src/main/java/com/example/java_springboot/entity/Reply.java
@@ -1,9 +1,12 @@
 package com.example.java_springboot.entity;
 
 import com.example.java_springboot.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -15,4 +18,22 @@ import javax.persistence.Table;
 @Table(name = "replys")
 public class Reply extends BaseEntity {
     private String content;
+    private int id_comment;
+    private int id_product;
+    private long id_account;
+
+    @ManyToOne
+    @JoinColumn(name = "id_comment", insertable = false, updatable = false)
+    @JsonIgnore
+    private Comment comment;
+
+    @ManyToOne
+    @JoinColumn(name = "id_account", insertable = false, updatable = false)
+    @JsonIgnore
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product", insertable = false, updatable = false)
+    @JsonIgnore
+    private Product product;
 }
